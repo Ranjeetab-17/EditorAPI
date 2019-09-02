020f1fc5798a86ae9eb20e035e5edf3fbea771fe
Ranjeetab-17 <ranjeetab17@gmail.com>
2019-09-02 23:07:44 +0530

    [RP] : Added Editor API controller

---
+++ controllers/EditorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EditorAPI.controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EditorController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetContent()
+        {
+            return Ok("Editor Works...");
+        }
+    }
+}
